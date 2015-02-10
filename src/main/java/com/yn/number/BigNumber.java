b754a24b8d830980985884bfa492a9654689f6c7
杨楠 <nanyang205380@sohu-inc.com>
2015-02-10 11:00:30 +0800

    增加大数相加

--- src/main/java/com/yn/number/BigNumber.java
+++ src/main/java/com/yn/number/BigNumber.java
@@ -13,23 +13,25 @@ import java.math.BigInteger;
  */
 public class BigNumber {
 	public static void main(String[] args) {
-//		String number1 = "96234623984325352868629323412456451245451215645";
-//		String number2 = "934593826459982136492361231242342332342353223123498";
+		String number1 = "96234623984325352868629323412456451245451215645";
+		String number2 = "934593826459982136492361231242342332342353223123498";
 //		multiply(number1, number2);
-//		BigDecimal b1 = new BigDecimal(new BigInteger(number1));
-//		BigDecimal b2 = new BigDecimal(new BigInteger(number2));
-//		System.out.println("api");
-//		System.out.println("大数相乘结果:" + b1.multiply(b2));
+		BigDecimal b1 = new BigDecimal(new BigInteger(number1));
+		BigDecimal b2 = new BigDecimal(new BigInteger(number2));
+		System.out.println("api");
+		System.out.println("大数相加:" + b1.add(b2));
 //		long start = System.currentTimeMillis();
-		String r = factorial(100);
+//		String r = factorial(100);
 //		System.out.println("耗时:"+(System.currentTimeMillis() - start));
 ////		long a = fun(10);
-		System.out.println("阶乘:"+r);
+//		System.out.println("阶乘:"+r);
 		
-		long r1 = power(2, 20);
-		System.out.println("幂运算:" + r1);
-		powerBigNumber(2, 64);
+//		long r1 = power(2, 20);
+//		System.out.println("幂运算:" + r1);
+//		powerBigNumber(2, 64);
 		
+		String rr = addBigNumber(number1, number2);
+		System.out.println("大数相加:"+rr);
 	}
 	
 	/**
@@ -50,7 +52,7 @@ public class BigNumber {
 		//逐位相乘
 		for(int i = 0; i<a.length; ++i) {
 			for(int j=0; j<b.length; ++j) {
-				c[i+j] += Integer.parseInt(String.valueOf(a[i])) * Integer.parseInt(String.valueOf(b[j])); 
+				c[i+j] += (a[i] - '0') * (b[j] - '0'); 
 			}
 		}
 		
@@ -135,5 +137,56 @@ public class BigNumber {
 		}
 		System.out.println("大数幂:" + c);
 		return c;
+	}
+	
+	/**
+	 * @author: YangNan(杨楠)  
+	 * @Title: addBigNumber   
+	 * @Description: 大数相加   
+	 */
+	public static String addBigNumber(String number1, String number2) {
+		
+		//字段串翻转
+		StringBuilder sb1 = new StringBuilder(number1);
+		StringBuilder sb2 = new StringBuilder(number2);
+		
+		char[] c1 = sb1.reverse().toString().toCharArray();
+		char[] c2 = sb2.reverse().toString().toCharArray();
+		int len1 = c1.length;
+		int len2 = c2.length;
+		
+		// 两数相加位数不会超过最大的位数 + 1
+		int len = len1 > len2 ? len1+1 : len2+1;
+		int[] r = new int[len];
+		
+		//逐位相加
+		for(int i=0; i< len; ++i) {
+			int cr1 = i >= len1 ? 0 : c1[i] - '0';
+			int cr2 = i >= len2 ? 0 : c2[i] - '0';
+			r[i] = cr1 + cr2;
+		}
+		
+		//计算进位
+		for(int i=0; i<len; ++i) {
+			int carry = r[i] / 10;
+			r[i] = r[i] % 10;
+			if(carry > 0)
+				r[i+1] += carry;
+		}
+		
+		//找出第一个不为0的最高为
+		int m = 0;
+		for(m=r.length-1; m>=0;) {
+			if(r[m] > 0)
+				break;
+			m--;
+		}
+		
+		StringBuilder s = new StringBuilder();
+		for(int i=0; i<=m; ++i) {
+			s.append(r[m-i]);
+		}
+		
+		return s.toString();
 	}
 }
